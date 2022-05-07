900e81945741fc4a8232a8255c34a87318323fd7
popaDaria <popadaria19@gmail.com>
2022-05-07 18:56:58 +0200

    first commit

---
+++ Models/FoodItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace TestServelessWebApp.Models
+{
+    public class FoodItem
+    {
+        [JsonPropertyName("id")]
+        public string ID { get; set; } = Guid.NewGuid().ToString("n");
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("person")]
+        public string Person { get; set; }
+        [JsonPropertyName("vegetarian")]
+        public bool Vegetarian { get; set; }
+        [JsonPropertyName("glutenfree")]
+        public bool Glutenfree { get; set; }
+    }
+}
